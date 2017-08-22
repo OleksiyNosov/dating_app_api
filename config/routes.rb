00eee831afae6e24011ec88bf5c344ac965a6eab
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-23 02:25:16 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, except: [:new, :edit]
-
-    resourse :profile, only: [:show, :create, :update] do
-      resourse :avatar, only: [:update]
+    resource :profile, only: [:show, :create, :update] do
+      resource :avatar, only: [:update]
     end
   end
 end
