a11053ea3b5f8472a82c9fee283f99888a602353
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-24 00:51:49 +0300

    Add avarar column to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823011303) do
+ActiveRecord::Schema.define(version: 20170823214837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 20170823011303) do
     t.float "lng"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "auth_tokens", "users"
