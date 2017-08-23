94f9da46473b3eb3c434955f562c10b90ec219fe
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-23 03:18:22 +0300

    Setup ApplicationDecorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,8 +1,9 @@
 class ApplicationDecorator < Draper::Decorator
-  # Define methods for all decorated objects.
-  # Helpers are accessed through `helpers` (aka `h`). For example:
-  #
-  #   def percent_amount
-  #     h.number_to_percentage object.amount, precision: 2
-  #   end
+  def as_json *args
+    super only: _only, methods: _methods
+  end
+
+  private
+  def _only() [] end
+  def _methods() [] end
 end
