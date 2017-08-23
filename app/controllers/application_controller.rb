0e8ff1ee67c19f872faa30bd594a19c8f5742ceb
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-23 03:00:06 +0300

    Setup ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :resource, :collection
+
+  def index
+    collection
+  end
+
+  def show
+    resource
+  end
+
+  def create
+    build_resource
+
+    resource.save!
+  end
+
+  def update
+    resource.update!
+  end
+
+  def destroy
+    resource.destroy!
+  end
 end
