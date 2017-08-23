fe5b98d825ac0439f5da88ff31a4f4bfefe9576b
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-23 03:01:52 +0300

    Fix gender in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  enum :gender, [:male, :female]
+  enum gender: [:male, :female]
 
   validates :email, presence: true, uniqueness: { case_sensetive: false }, email: true
 end
