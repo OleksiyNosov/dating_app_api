abf5f0d1c294b2daced4eb5804f6253fcea8f4d1
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-23 06:15:58 +0300

    Add routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
+    resource :session, only: [:create, :destroy]
+
     resource :profile, only: [:show, :create, :update] do
       resource :avatar, only: [:update]
     end
