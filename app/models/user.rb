8fc07c750538b30538b7e3c6674586c3c8dd35c0
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-24 03:14:14 +0300

    Setup User model for using avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ApplicationRecord
 
   has_many :auth_tokens, dependent: :destroy
 
+  has_attached_file :avatar, styles: { thumb: '300x300#' }
+
   enum gender: [:male, :female]
 
   validates :email, presence: true, uniqueness: { case_sensetive: false }, email: true
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/ 
 
   def create_auth_token
     AuthToken.create value: SecureRandom.uuid, user_id: id
