519afb26891670039d7dbe1b220e8ce76dc597cd
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-24 17:18:33 +0300

    Create PlacesController

---
+++ app/controllers/api/places_controller.rb
@@ -0,0 +1,18 @@
+class Api::PlacesController < ApplicationController
+  private
+  def build_resource
+    @place = Place.new resource_params
+  end
+
+  def collection
+    @places ||= Place.all
+  end
+
+  def resource
+    @place ||= Place.find params[:id]
+  end
+
+  def resource_params
+    params.require(:place).permit(:name, :city, :tags, :lat, :lng)
+  end
+end
