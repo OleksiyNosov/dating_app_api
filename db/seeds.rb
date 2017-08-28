3b84cde92cb4381b41ecd2b8eaae51e737607d09
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-28 08:00:01 +0300

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require File.join(Rails.root, 'db', 'seeds_helper')
+
+puts "Create Rng"
+
+users_numb       =   100
+places_numb      =   100
+place_users_numb = 1_000
+
+puts "Create #{ users_numb } users"
+users = users_numb.times.map do
+  User.create \
+    email:      Faker::Internet.email,
+    password:   Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name:  Faker::Name.last_name,
+    birthday:   Faker::Date.birthday(18, 30).iso8601,
+    gender:     gen_gender,
+    lat:        gen_lat,
+    lng:        gen_lng
+end
+
+puts "Create #{ places_numb } places"
+places = places_numb.times.map do
+  Place.create \
+    name:     Faker::Pokemon.name,
+    city:     Faker::Pokemon.location,
+    tags:     gen_place_tags,
+    lat:      gen_lat,
+    lng:      gen_lng,
+    place_id: gen_place_id
+end
+
+puts "Create #{ place_users_numb } place_user (users reviews)"
+place_users_numb.times do
+  PlaceUser.create \
+    user_id:  users.sample.id,
+    place_id: places.sample.id,
+    rating:   gen_rating,
+    review:   gen_quote
+end
+
