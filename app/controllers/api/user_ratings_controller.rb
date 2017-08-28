93ca9fe4f58bce6eb04080d0e774a5d0e0862cf4
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-28 22:35:33 +0300

    Create UserRatingsController

---
+++ app/controllers/api/user_ratings_controller.rb
@@ -0,0 +1,16 @@
+class Api::UserRatingsController < ApplicationController
+  before_action -> { set_decorator_context user_ratings: true }
+
+  private
+  def collection
+    parent
+  end
+
+  def parent
+    parent_key = params.keys.detect { |k| k.match /(\w+)_id/ }
+
+    parent_class = parent_name[0..-4].capitalize.constantize
+
+    @parent = parent_class.find params[parent_key]
+  end
+end
