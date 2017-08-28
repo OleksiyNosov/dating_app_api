6447824434969a53adea77230ece517f7d279f07
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-28 08:27:46 +0300

    Create recalculation of place overall rating

--- app/models/place_user.rb
+++ app/models/place_user.rb
@@ -4,4 +4,11 @@ class PlaceUser < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :place_id }
   validates :rating, inclusion: { in: 1..5 }
+
+  after_commit :recalculate_overall_rating, on: [:create, :update]
+
+  private
+  def recalculate_overall_rating
+    place.update! overall_rating: place.place_users.average(:rating) 
+  end
 end
