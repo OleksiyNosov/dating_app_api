8506c418332983d07794b8c1fef7bda8652b2823
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-29 07:16:37 +0300

    Add tests for Place model

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
+  it { should be_an ApplicationRecord }
+
+  it { should have_many(:place_users) }
+
+  it { should have_many(:users).through(:place_users) }
 end
