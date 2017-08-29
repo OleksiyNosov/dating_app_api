7cb6726c2934aa63e2c4b523fdb618343320ef0d
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-29 07:47:36 +0300

    Add tests for PlaceUser model

--- spec/models/place_user_spec.rb
+++ spec/models/place_user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PlaceUser, type: :model do
+  it { should be_an ApplicationRecord }
+
+  it { should belong_to(:user) }
+
+  it { should belong_to(:place) }
+
+  # it { should validate_uniqueness_of(:user_id).scoped_to(:place_id) }
+
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
 end
