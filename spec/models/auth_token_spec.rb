576d475c9542c40266dc0ab8f8c60109af142a38
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-29 07:29:09 +0300

    Add tests for AuthToken model

--- spec/models/auth_token_spec.rb
+++ spec/models/auth_token_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AuthToken, type: :model do
+  it { should be_an ApplicationRecord }
+
+  it { should belong_to(:user) }
+
+  it { should validate_presence_of :value }
+
+  it { should validate_uniqueness_of :value }
 end
