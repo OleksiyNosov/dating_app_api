8ef1d0595ed9622bd7b266f9f6271348715b6e5a
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-01 00:42:59 +0300

    Create tests for UsersController

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Api::UsersController, type: :controller do
+  it { should be_an ApplicationController }
+
+  describe '#index' do
+    before { expect(User).to receive(:all).and_return(:collection) }
+
+    its(:collection) { should eq :collection }
+
+    before { process :index, method: :get, format: :json }
+
+    it { should render_template :index }
+  end
+
+  describe '#show' do
+    let(:params) { { id: '1' } }
+
+    let(:user) { stub_model User }
+
+    before { expect(User).to receive(:find).with('1').and_return(user) }
+
+    its(:resource) { should eq user }
+
+    before { process :show, method: :get, params: params, format: :json }
+
+    it { should render_template :show }
+  end
+end
