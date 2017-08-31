2c46ebdbb71a06a1bf4665d5cd07cd547300bdeb
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-01 01:31:39 +0300

    Create tests for ProfilesController

---
+++ spec/controllers/api/profiles_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe Api::ProfilesController, type: :controller do
+  it { should be_an ApplicationController }
+
+  describe '#show' do
+    let(:user) { stub_model User }
+
+    before { expect(subject).to receive(:authenticate) }
+    
+    before { expect(subject).to receive(:current_user).and_return(user) }
+
+    its(:resource) { should eq user }
+    
+    before { process :show, method: :get, format: :json }
+
+    it { should render_template :show }
+  end
+
+  describe '#create' do
+    let(:user_params) { { email: 'test@test.com', password: 'qwerty', password_confirmation: 'qwerty' } }
+
+    let(:params) { { user: user_params } }
+
+    let(:user) { stub_model User }
+
+    before { expect(User).to receive(:new).with(permit! user_params).and_return(user) }
+
+    before { expect(user).to receive(:save!) }
+
+    before { process :create, method: :post, params: params, format: :json }
+
+    it { should render_template :create }
+  end
+
+  describe '#update' do
+    let(:user_params) { { first_name: 'test', last_name: 'test' } }
+
+    let(:params) { { user: user_params } }
+
+    let(:user) { stub_model User }
+
+    before { expect(subject).to receive(:authenticate) }
+
+    before { expect(subject).to receive(:resource).and_return(user) }
+
+    before { expect(user).to receive(:update!).with(permit! user_params) }
+
+    before { process :update, method: :patch, params: params, format: :json }
+
+    it { should render_template :update }
+  end
+end
