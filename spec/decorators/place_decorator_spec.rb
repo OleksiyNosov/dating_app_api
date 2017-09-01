f644056b3585119dd55cbc2ce348bb447e20635e
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-01 06:43:35 +0300

    Create tests for PlaceDecorator

---
+++ spec/decorators/place_decorator_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe PlaceDecorator do
+  let(:place) do 
+    stub_model Place, 
+    id: 3, 
+    name: 'Mafia', 
+    city: 'Vinnitsia',
+    place_id: '', 
+    tags: ['beer', 'pizza'], 
+    overall_rating: 4.6,
+    lat: 136.5,
+    lng: 30.9
+  end
+
+  describe '#as_json' do
+    context do
+      subject { place.decorate }
+
+      its('as_json.symbolize_keys') do
+        should eq \
+        id: 3, 
+        name: 'Mafia', 
+        city: 'Vinnitsia',
+        place_id: '', 
+        tags: ['beer', 'pizza'], 
+        overall_rating: 4.6,
+        coords: { lat: 136.5, lng: 30.9 }
+      end
+    end
+
+    context 'place_user_ratings' do
+
+      subject { place.decorate(context: { place_user_ratings: true }) }
+
+      its('as_json.symbolize_keys') do
+        should eq \
+        id: 3, 
+        name: 'Mafia', 
+        city: 'Vinnitsia',
+        place_id: '', 
+        tags: ['beer', 'pizza'], 
+        overall_rating: 4.6,
+        coords: { lat: 136.5, lng: 30.9 },
+        ratings: []
+      end
+    end
+  end
+end
