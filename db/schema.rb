68c75f8fec1676944683bedd6eecc1f8eac59c2a
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-02 19:42:46 +0300

    Add extensions cube and earthdistance

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830232220) do
+ActiveRecord::Schema.define(version: 20170902163412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "cube"
+  enable_extension "earthdistance"
 
   create_table "auth_tokens", force: :cascade do |t|
     t.string "value"
