a6b2c83460ab62f7522ff52e773992764d659db8
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-05 06:22:08 +0300

    Add clomn expired_at to auth_tokens table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904051834) do
+ActiveRecord::Schema.define(version: 20170905032015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170904051834) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expired_at"
     t.index ["user_id"], name: "index_auth_tokens_on_user_id"
     t.index ["value"], name: "index_auth_tokens_on_value", unique: true
   end
