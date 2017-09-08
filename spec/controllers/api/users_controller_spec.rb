0599a58c77ffb467bbcb7f4254222fd5d60d80b3
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-08 06:44:30 +0300

    Refacor test for UsersController

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -6,10 +6,6 @@ RSpec.describe Api::UsersController, type: :controller do
   describe '#index' do
     before { sign_in }
 
-    before { expect(User).to receive(:all).and_return(:collection) }
-
-    its(:collection) { should eq :collection }
-
     before { process :index, method: :get, format: :json }
 
     it { should render_template :index }
@@ -18,16 +14,35 @@ RSpec.describe Api::UsersController, type: :controller do
   describe '#show' do
     let(:params) { { id: '1' } }
 
-    let(:user) { double }
-
-    before { sign_in user }
-
-    before { expect(User).to receive(:find).with('1').and_return(user) }
-
-    its(:resource) { should eq user }
+    before { sign_in }
 
     before { process :show, method: :get, params: params, format: :json }
 
     it { should render_template :show }
+  end
+
+  describe '#collection' do
+    before { expect(User).to receive(:all).and_return(:collection) }
+
+    its(:collection) { should eq :collection }
+  end
+
+  describe '#resource' do
+    let(:params) { '1' }
+
+    let(:user) { stub_model User }
+
+    before do 
+      #
+      # params[:id] -> 1
+      #
+      expect(subject).to receive(:params) do
+        double.tap { |a| expect(a).to receive(:[]).with(:id).and_return(params) }
+      end
+    end
+    
+    before { expect(User).to receive(:find).with(params).and_return(user) }
+
+    its(:resource) { should eq user }
   end
 end
