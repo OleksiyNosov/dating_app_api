858c839b17a4821e131c7408f8ec269b4b611e86
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-08 10:55:26 +0300

    Refactor search

--- app/searchers/place_searcher.rb
+++ app/searchers/place_searcher.rb
@@ -21,14 +21,34 @@ class PlaceSearcher < ApplicationSearcher
   def search_by_range range
     return unless range
 
-    @results = @results.where build_query_by_range(@params[:user], km_to_m(range.to_i), 'places')
+    @results = @results.select build_select_query(@params[:user], 'places', 'distance')
+
+    @results = @results.where build_where_query_by_range(@params[:user], km_to_miles(range.to_f), 'places')
+
+    @results = @results.order 'distance'
   end
 
-  def build_query_by_range center, range, table
-    earth_box = "earth_box(ll_to_earth(#{ center[:lat] }, #{ center[:lng] }), #{ range })"
+  def build_select_query center, table, field 
+    center_point = build_point_from center[:lng], center[:lat]
 
-    location = "ll_to_earth(#{ table }.lat, #{ table }.lng)"
+    table_point = build_point_for table
 
-    "#{ earth_box } @> #{ location }"
+    "*, #{ center_point } <@> #{ table_point }::point AS #{ field }"
+  end
+
+  def build_where_query_by_range center, range, table
+    center_point = build_point_from center[:lng], center[:lat]
+
+    location = build_point_for table
+
+    "#{ center_point } <@> #{ location } <= #{ range }"
+  end
+
+  def build_point_from lng, lat
+    "point(#{ lng }, #{ lat })"
+  end
+
+  def build_point_for table
+    "point(#{ table }.lng, #{ table }.lat)"
   end
 end
