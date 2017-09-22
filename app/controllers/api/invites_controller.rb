ed3bad9ada5cc3687b1ee949360c4dff1a7a162d
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-22 07:14:32 +0300

    Add different contexts

--- app/controllers/api/invites_controller.rb
+++ app/controllers/api/invites_controller.rb
@@ -1,4 +1,8 @@
 class Api::InvitesController < ApplicationController
+  before_action -> { set_proper_decorator_context }, only: [:index, :show]
+  before_action -> { set_decorator_context(with_user: true) }, only: [:create]
+  before_action -> { set_decorator_context(with_event: true) }, only: [:update]
+
   private
   def build_resource
     @invite = Invite.create resource_params.merge event: parent, respond: :no_respond 
@@ -9,20 +13,34 @@ class Api::InvitesController < ApplicationController
   end
 
   def resource
-    @invite ||= Event.find params[:id]
+    @invite = Invite.find params[:id]
+  end
+
+  def profile_is_parent?
+    !params[:event_id]
   end
 
   def resource_params
-    params.require(:invite).permit(:user_id)
+    if profile_is_parent?
+      params.require(:invite).permit(:respond)
+    else
+      params.require(:invite).permit(:user_id)
+    end
   end
 
   def parent
-    puts "params #{ params.to_s }"
-
-    if params[:event_id] 
-      @parent = Event.find params[:event_id]
-    else 
+    if profile_is_parent?
       @parent = current_user
+    else 
+      @parent = Event.find params[:event_id]
+    end
+  end
+
+  def set_proper_decorator_context
+    if profile_is_parent?
+      set_decorator_context with_event: true
+    else
+      set_decorator_context with_user: true
     end
   end
 end
