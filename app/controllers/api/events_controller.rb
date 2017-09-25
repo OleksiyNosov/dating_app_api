46489d2136b6b7ba9689b8a5b0e051531fef40ff
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-25 23:58:25 +0300

    Refactor events controller actions create and update

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,5 +1,6 @@
 class Api::EventsController < ApplicationController
   before_action :validate_author, only: [:update, :delete]
+  before_action :add_new_invites, only: [:update]
   before_action -> { set_decorator_context(full: true) }, only: [:show, :create, :update]
   before_action -> { set_decorator_context(short: true) }, only: [:index]
 
@@ -15,14 +16,18 @@ class Api::EventsController < ApplicationController
   def build_resource
     @event = Event.new resource_params.merge user_id: current_user.id
 
-    params[:event][:invites].each { |id| @event.create_invite_if_not_exist User.find(id.to_i) }
+    add_new_invites
   end
 
   def resource_params
-    params.require(:event).permit(:place_id, :title, :description, :kind, :start_time, :invites)
+    params.require(:event).permit(:place_id, :title, :description, :kind, :start_time)
   end
 
   def validate_author
     head :forbidden if resource.user != current_user   
+  end
+
+  def add_new_invites
+    params[:event][:invites].uniq.map(&:to_i).each { |id| resource.new_invite_if_not_exist User.find id }
   end
 end
