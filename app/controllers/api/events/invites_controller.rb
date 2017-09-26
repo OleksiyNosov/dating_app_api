87fdd28dc9105f90872e96a1ee170a8a00cf6320
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-26 15:30:25 +0300

    Update to use superclass

--- app/controllers/api/events/invites_controller.rb
+++ app/controllers/api/events/invites_controller.rb
@@ -1,17 +1,9 @@
-class Api::Events::InvitesController < ApplicationController
+class Api::Events::InvitesController < Api::InvitesController 
   before_action -> { set_decorator_context(with_user: true) }, only: [:index, :show, :create]
 
   private
   def build_resource
     @invite = Event.invites.build resource_params
-  end
-
-  def collection
-    @invites ||= parent.invites
-  end
-
-  def resource
-    @invite ||= parent.find params[:id]
   end
 
   def resource_params
