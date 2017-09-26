a973af322029bc191ee9d85a7fd39407b5a72ad0
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-26 05:39:43 +0300

    Add search by range

--- app/searchers/place_searcher.rb
+++ app/searchers/place_searcher.rb
@@ -1,6 +1,4 @@
 class PlaceSearcher < ApplicationSearcher
-  include SimpleMathOperations
-
   private
   def initialize_results
     @results = Place.all
@@ -21,34 +19,8 @@ class PlaceSearcher < ApplicationSearcher
   def search_by_range range
     return unless range
 
-    @results = @results.select build_select_query(@params[:user], 'places', 'distance')
-
-    @results = @results.where build_where_query_by_range(@params[:user], km_to_miles(range.to_f), 'places')
-
-    @results = @results.order 'distance'
-  end
-
-  def build_select_query center, table, field 
-    center_point = build_point_from center[:lng], center[:lat]
-
-    table_point = build_point_for table
-
-    "*, #{ center_point } <@> #{ table_point }::point AS #{ field }"
-  end
-
-  def build_where_query_by_range center, range, table
-    center_point = build_point_from center[:lng], center[:lat]
-
-    location = build_point_for table
-
-    "#{ center_point } <@> #{ location } <= #{ range }"
-  end
-
-  def build_point_from lng, lat
-    "point(#{ lng }, #{ lat })"
-  end
-
-  def build_point_for table
-    "point(#{ table }.lng, #{ table }.lat)"
+    @results = @results.select(build_select_query(@params[:current_user], 'places', 'distance'))
+                       .where(build_where_query_by_range(@params[:current_user], km_to_miles(range.to_f), 'places'))
+                       .order('distance')
   end
 end
