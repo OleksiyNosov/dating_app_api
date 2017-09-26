07dc1ba357a703f50b1cd813d689fa1baf23add4
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-26 15:21:46 +0300

    Move code to proper folders

---
+++ app/controllers/api/events/invites_controller.rb
@@ -0,0 +1,24 @@
+class Api::Events::InvitesController < ApplicationController
+  before_action -> { set_decorator_context(with_user: true) }, only: [:index, :show, :create]
+
+  private
+  def build_resource
+    @invite = Event.invites.build resource_params
+  end
+
+  def collection
+    @invites ||= parent.invites
+  end
+
+  def resource
+    @invite ||= parent.find params[:id]
+  end
+
+  def resource_params
+    params.require(:invite).permit(:user_id)
+  end
+
+  def parent
+    @parent ||= Event.find params[:event_id]
+  end
+end
---
+++ app/controllers/api/profiles/invites_controller.rb
@@ -0,0 +1,16 @@
+class Api::Profiles::InvitesController < ApplicationController
+  before_action -> { set_decorator_context(with_event: true) }, only: [:index, :show, :update]
+
+  private
+  def collection
+    @invites = current_user.invites
+  end
+
+  def resource
+    @invite = current_user.find params[:id]
+  end
+
+  def resource_params
+    params.require(:invite).permit(:respond)
+  end
+end
