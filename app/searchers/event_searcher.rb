233b55bd39ee04fa6c34f0c93c4415341cabf8de
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-26 05:39:13 +0300

    Delete useless code

--- app/searchers/event_searcher.rb
+++ app/searchers/event_searcher.rb
@@ -9,12 +9,25 @@ class EventSearcher < ApplicationSearcher
   end
 
   def search_by_end_date end_date
-    search_between_dates if end_date.present? && start_date.blank?
+    search_between_dates if end_date.present? && @params[:start_date].blank?
+  end
+
+  def search_by_range range
+    @results = @results.joins(:place)
+                       .select(build_select_query(@params[:current_user], 'places', 'distance'))
+                       .where(build_where_query_by_range(@params[:current_user], km_to_miles(range.to_f), 'places'))
+                       .order('distance')
+                                          
   end
 
   def search_between_dates
-    start_date = @params[:start_date].present? ? @params[:start_date].to_time : Time.now
-    end_date   = @params[:end_date].present?   ? @params[:end_date].to_time   : Time.now + 1.day
+    if @params[:start_date].present?
+      start_date = @params[:start_date].to_time 
+      end_date = @params[:end_date].present? ? @params[:end_date].to_time : start_date + 1.day
+    else
+      end_date = @params[:end_date].to_time 
+      start_date = end_date - 1.day
+    end
 
     @results = @results.where(start_time: start_date..end_date)
   end
