a06e3f4590a0bfb392318a47723707b2844d02dd
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-28 03:17:57 +0300

    Refactor search_by_range and search_between_dates

--- app/searchers/event_searcher.rb
+++ app/searchers/event_searcher.rb
@@ -1,7 +1,7 @@
 class EventSearcher < ApplicationSearcher
   private
   def initialize_results
-    @results = Event.where(kind: :public_event, user_id: @params[:current_user].id)
+    @results = Event.all
   end
 
   def search_by_start_date start_date
@@ -15,20 +15,20 @@ class EventSearcher < ApplicationSearcher
   def search_by_range range
     @results = @results
       .joins(:place)
-      .select(build_select_query(@params[:current_user], 'places', 'distance'))
-      .where(build_where_query_by_range(@params[:current_user], km_to_miles(range.to_f), 'places'))
-      .order('distance')                                          
+      .select("events.*, earth_distance(ll_to_earth(#{ user.lat }, #{ user.lng }), ll_to_earth(places.lat, places.lng)) AS distance")
+      .where("earth_box(ll_to_earth(?, ?), ?) @> ll_to_earth(places.lat, places.lng)", user.lat, user.lng, range.to_f)
+      .order('distance')
   end
 
   def search_between_dates
-    if @params[:start_date].present?
-      start_date = @params[:start_date].to_time 
-      end_date = @params[:end_date].present? ? @params[:end_date].to_time : start_date + 1.day
-    else
-      end_date = @params[:end_date].to_time 
-      start_date = end_date - 1.day
-    end
-
     @results = @results.where(start_time: start_date..end_date)
+  end
+
+  def start_date
+    @start_date ||= @params[:start_date].present? ? @params[:start_date].to_time : Time.now
+  end
+
+  def end_date
+    @end_date ||= @params[:end_date].present? ? @params[:end_date].to_time : start_date + 1.day
   end
 end
