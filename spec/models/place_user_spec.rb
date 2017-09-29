a59c4576c3dc8b045a30b0e74655ae83da831334
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-29 03:23:59 +0300

    Cover recalculate_overall_rating from PlaceUser model

--- spec/models/place_user_spec.rb
+++ spec/models/place_user_spec.rb
@@ -7,7 +7,30 @@ RSpec.describe PlaceUser, type: :model do
 
   it { should belong_to(:place) }
 
-  # it { should validate_uniqueness_of(:user).scoped_to(:place) }
-
   it { should validate_inclusion_of(:rating).in_range(1..5) }
+
+  describe '#recalculate_overall_rating' do
+    let(:overall_rating) { 3.5 }
+
+    let(:place_users) { double }
+
+    let(:place) { stub_model Place }
+
+    before { expect(subject).to receive(:place).and_return(place) }
+
+    before do 
+      #
+      # -> place.place_users.average
+      #
+      expect(place).to receive(:place_users) do
+        double.tap { |a| expect(a).to receive(:average).with(:rating).and_return(overall_rating) }
+      end 
+    end
+
+    before { expect(subject).to receive(:place).and_return(place) }
+
+    before { expect(place).to receive(:update!).with(overall_rating: overall_rating).and_return(place) }
+
+    its(:recalculate_overall_rating) { should eq place }
+  end
 end
