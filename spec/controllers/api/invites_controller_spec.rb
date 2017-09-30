e61686026fb97aa44dec305fa8aa23dd26a1ff45
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-30 03:42:46 +0300

    Add test for collection

--- spec/controllers/api/invites_controller_spec.rb
+++ spec/controllers/api/invites_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Api::InvitesController, type: :controller do
   it { should be_an ApplicationController }
+
+  describe '#collection' do
+    before do
+      #
+      # -> parent.invites
+      # 
+      expect(subject).to receive(:parent) do
+        double.tap { |a| expect(a).to receive(:invites).and_return(:collection) }
+      end       
+    end
+
+    its(:collection) { should eq :collection }
+  end
 end
