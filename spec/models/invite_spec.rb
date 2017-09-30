05417f29d117feb471fc6d868e798c3c02c3986f
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-30 19:06:04 +0300

    Create tests for invite

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should be_an ApplicationRecord }
+
+  it { should belong_to :event }
+
+  it { should belong_to :user }
+
+  it { should validate_presence_of :event }
+
+  it { should validate_presence_of :user }
+
+  it { should define_enum_for(:respond).with([:no_respond, :attend, :not_attend]) }
 end
