01a1bf974511a993a94aba1ff777a3b1b0cce3f8
OleksiyNosov <oleksiynosov@gmail.com>
2017-09-30 05:51:11 +0300

    Create file for test application controller

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper' 
+
+RSpec.describe ApplicationController, type: :controller do
+  describe '#authenticate' do
+    before { expect(subject).to receive(:authenticate_or_request_with_http_token).and_return :result }
+
+    its(:authenticate) { should eq :result }
+  end
+end
