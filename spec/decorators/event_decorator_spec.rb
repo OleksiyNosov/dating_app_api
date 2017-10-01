ec4ba0e57d22bf50b442f2c0f485095f3cbc0b7f
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-02 01:21:30 +0300

    Create tests for event decorator

---
+++ spec/decorators/event_decorator_spec.rb
@@ -0,0 +1,166 @@
+require 'rails_helper'
+
+RSpec.describe EventDecorator do
+  let(:event) do
+    stub_model Event, id: 5, user_id: 2, place_id: 3, 
+    title: 'Party', description: 'Blackout 1 love',
+    kind: 'public_event', start_time: Time.new(2017, 9, 30, 23, 36) 
+  end
+
+  subject { event.decorate }
+
+  describe '#date' do
+    before do
+      #
+      # => start_time.strftime
+      #
+      expect(subject).to receive(:start_time) do
+        double.tap { |a| expect(a).to receive(:strftime).with('%F').and_return '2017-9-30' }
+      end
+    end
+
+    its(:date) { should eq '2017-9-30' }
+  end
+
+  describe '#time' do
+    before do
+      #
+      # => start_time.strftime
+      #
+      expect(subject).to receive(:start_time) do
+        double.tap { |a| expect(a).to receive(:strftime).with('%H:%M').and_return '23:36' }
+      end
+    end
+
+    its(:time) { should eq '23:36' }
+  end
+
+  describe '#author' do  
+    let(:user) { stub_model User }
+
+    before { expect(subject).to receive(:user).and_return user }
+
+    its(:author) { should eq user }
+  end
+
+  describe '#people_attended_count' do
+    before do
+      #
+      # => object.invites.attend.count
+      #
+      expect(subject).to receive(:object) do
+        double.tap do |a|
+          expect(a).to receive(:invites) do
+            double.tap do |b|
+              expect(b).to receive(:attend) do
+                double.tap { |c| expect(c).to receive(:count).and_return 4 }
+              end
+            end
+          end
+        end
+      end
+    end
+
+    its(:people_attended_count) { should eq 4 }
+  end
+
+  describe '#people_attended' do
+    before do
+      #
+      # => object.invites.attend.map
+      #
+      expect(subject).to receive(:object) do
+        double.tap do |a|
+          expect(a).to receive(:invites) do
+            double.tap do |b|
+              expect(b).to receive(:attend) do
+                double.tap { |c| expect(c).to receive(:map).and_return :people_attended }
+              end
+            end
+          end
+        end
+      end
+    end
+
+    its(:people_attended) { should eq :people_attended }
+  end
+
+  describe '#invites' do
+    before do
+      #
+      # => object.invites.decorate
+      #
+      expect(subject).to receive(:object) do
+        double.tap do |a|
+          expect(a).to receive(:invites) do
+            double.tap { |b| expect(b).to receive(:decorate).with(context: { with_user: true }).and_return :invites } 
+          end
+        end
+      end
+    end
+
+    its(:invites) { should eq :invites }
+  end
+
+  describe '#as_json' do
+    context 'short' do
+      subject { event.decorate context: { short: true } }
+
+      let(:place) { stub_model Place }
+
+      before { expect(subject).to receive(:place).and_return place }
+
+      before { expect(subject).to receive(:date).and_return '2017-9-30' }
+
+      before { expect(subject).to receive(:time).and_return '23:36' }
+
+      its(:'as_json.symbolize_keys') do
+        should eq \
+        id: 5,
+        kind: 'public_event',
+        title: 'Party',
+        description: 'Blackout 1 love',
+        place: place,
+        date: '2017-9-30',
+        time: '23:36'
+      end
+    end
+
+    context 'full' do
+      subject { event.decorate context: { full: true } }
+
+      let(:place) { stub_model Place }
+
+      let(:author) { stub_model User }
+
+      before { expect(subject).to receive(:place).and_return place }
+
+      before { expect(subject).to receive(:author).and_return author }
+
+      before { expect(subject).to receive(:date).and_return '2017-9-30' }
+
+      before { expect(subject).to receive(:time).and_return '23:36' }
+
+      before { expect(subject).to receive(:people_attended_count).and_return 4 }
+
+      before { expect(subject).to receive(:people_attended).and_return :people_attended }
+
+      before { expect(subject).to receive(:invites).and_return :invites }
+
+      its(:'as_json.symbolize_keys') do
+        should eq \
+        id: 5,
+        kind: 'public_event',
+        title: 'Party',
+        description: 'Blackout 1 love',
+        place: place,
+        date: '2017-9-30',
+        time: '23:36',
+        author: author,
+        people_attended_count: 4,
+        people_attended: :people_attended,
+        invites: :invites
+      end
+    end
+  end
+end
