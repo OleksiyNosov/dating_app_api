5e3a8909d413134b151b92d47063659e4baf3a20
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-06 04:40:43 +0300

    Start using factories for users and places

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,16 @@
 require File.join(Rails.root, 'db', 'seeds_helper')
 
-puts "Create Rng"
+puts 'Create Rng'
 
 users_numb       =   100
 places_numb      =   100
 place_users_numb = 1_000
 
 puts "Create #{ users_numb } users"
-users = users_numb.times.map do
-  User.create \
-    email:      Faker::Internet.email,
-    password:   Faker::Internet.password,
-    first_name: Faker::Name.first_name,
-    last_name:  Faker::Name.last_name,
-    birthday:   Faker::Date.birthday(18, 30).iso8601,
-    gender:     gen_gender,
-    lat:        gen_lat,
-    lng:        gen_lng
-end
+users = FactoryGirl.create_list :user, users_numb
 
 puts "Create #{ places_numb } places"
-places = places_numb.times.map do
-  Place.create \
-    name:     Faker::Pokemon.name,
-    city:     Faker::Pokemon.location,
-    tags:     gen_place_tags,
-    lat:      gen_lat,
-    lng:      gen_lng,
-    place_id: gen_place_id
-end
+places = FactoryGirl.create_list :place, places_numb
 
 puts "Create #{ place_users_numb } place_user (users reviews)"
 place_users_numb.times do
