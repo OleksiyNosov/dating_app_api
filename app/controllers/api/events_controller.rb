da70864825c48b4e8bb6ae21f8040e67bed9b84e
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-06 06:03:13 +0300

    Update code due to creation of EventService

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -14,9 +14,7 @@ class Api::EventsController < ApplicationController
   end
 
   def build_resource
-    @event = Event.new resource_params.merge user_id: current_user.id
-
-    add_new_invites
+    @event = EventService.new(resource_params, current_user, params[:event][:invites]).build_event
   end
 
   def resource_params
@@ -25,9 +23,5 @@ class Api::EventsController < ApplicationController
 
   def validate_author
     head :forbidden if resource.user != current_user   
-  end
-
-  def add_new_invites
-    params[:event][:invites].uniq.map(&:to_i).each { |id| resource.new_invite_if_not_exist User.find id }
   end
 end
