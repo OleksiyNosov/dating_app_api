34112cb6389c208b84f8116b0d2d7f3a34b05a3a
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-08 01:53:59 +0300

    Fix EventBuilder

--- app/services/event_builder.rb
+++ app/services/event_builder.rb
@@ -1,4 +1,4 @@
-class EventBuilder 
+class EventBuilder
   def initialize resource_params, user, invites
     @resource_params = resource_params.merge user_id: user.id
 
@@ -6,15 +6,15 @@ class EventBuilder
   end
 
   def build_event
-    @event = Event.new resource_params
-  
+    @event = Event.new @resource_params
+
     add_invites_to_event
-  
+
     @event
   end
 
   private
   def add_invites_to_event
-    invites.each { |id| @event.new_invite_if_not_exist User.find id }
+    @invites.each { |id| @event.new_invite_if_not_exist User.find id }
   end
 end
