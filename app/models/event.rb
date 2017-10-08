243afdd4eb147f0288d313a339a806287220da17
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-08 05:40:45 +0300

    Add has_many people_attend in Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,8 @@ class Event < ApplicationRecord
   belongs_to :user
 
   has_many :invites, dependent: :destroy
+  has_many :invites_attend, -> { attend }, class_name: :Invite
+  has_many :people_attend, through: :invites_attend, class_name: :User, source: :user
 
   validates :place, :user, :title, presence: true
 
