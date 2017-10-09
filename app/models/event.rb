2514583b762b100ee2be220bb05b8506dabc8256
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-09 22:53:05 +0300

    Delete useless code

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,8 +9,4 @@ class Event < ApplicationRecord
   has_many :people_attend, through: :invites_attend, class_name: :User, source: :user
 
   validates :place, :user, :title, presence: true
-
-  def new_invite_if_not_exist invited_user
-    invites.build user: invited_user unless invites.find_by user: invited_user
-  end
 end
