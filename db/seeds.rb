510fe079c13d3e809a5a18c8ea4964cb2c66588d
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-09 22:42:56 +0300

    Update generation of more complex associations to be more simple

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ users = FactoryGirl.create_list :user, users_numb
 puts "Create #{ places_numb } places"
 places = FactoryGirl.create_list :place, places_numb
 
-puts "Create #{ place_users_numb } place_users (user reviews)"
-combinator = Combinator.new places, users
+puts "Create #{ place_users_numb } place_users (user reviews)" # TODO: Rewrite with factories that can handle complex associations
+pairs = places.product(users).shuffle
 
 place_users = Array.new place_users_numb do
-  pair = combinator.delete_random_pair
+  pair = pairs.pop
 
   FactoryGirl.create(:place_user, place: pair[0], user: pair[1])
 end
@@ -26,11 +26,11 @@ end
 puts "Create #{ events_numb } events"
 events = Array.new(events_numb) { FactoryGirl.create(:event, place: places.sample, user: users.sample) }
 
-puts "Create #{ invites_numb } invites"
-combinator = Combinator.new events, users
+puts "Create #{ invites_numb } invites" # TODO: Rewrite with factories that can handle complex associations
+pairs = events.product(users).shuffle
 
 invites = Array.new invites_numb do
-  pair = combinator.delete_random_pair
+  pair = pairs.pop
 
   FactoryGirl.create(:invite, event: pair[0], user: pair[1])
 end
