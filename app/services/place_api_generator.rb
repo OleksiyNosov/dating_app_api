ba45637f1ed578db0474bae8235ebafbcc05bc9a
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-10 04:47:02 +0300

    Add private attibutes, symbolized names in hash and refactor code

--- app/services/place_api_generator.rb
+++ app/services/place_api_generator.rb
@@ -6,49 +6,45 @@ class PlaceApiGenerator
   def download_and_create
     download_data
 
-    create_if_not_exist
+    create
   end
 
-  def download_and_create_to_list
+  def download_and_create_as_list
     Array.wrap download_and_create
   end
 
   private
+  attr_reader :data, :city
+
   def download_data
-    return unless @city && @city.present?
+    return unless city && city.present?
 
-    @data_list = JSON.parse(open("https://restcountries.eu/rest/v2/capital/#{ @city }").read) rescue return
+    raw_data = open("https://restcountries.eu/rest/v2/capital/#{ city }").read rescue return
+
+    @data = JSON.parse(raw_data, symbolize_names: true).first
   end
 
-  def create_if_not_exist
-    return [] if @data_list.nil? || @data_list[0]['capital'] != @city
+  def create
+    return if data.nil? || data[:capital].downcase != city.downcase
 
-    create(@data_list[0]) unless exits @data_list[0]
-  end
-
-  def exits data
-    PlaceSearcher.search(city: @city).any?
-  end
-
-  def create data
     Place.create! \
-      name: data['name'],
-      city: data['capital'],
-      tags: generate_tags(data),
-      lat: data['latlng'][0],
-      lng: data['latlng'][1],
-      overall_rating: generate_rating(data),
+      name: data[:name],
+      city: data[:capital],
+      tags: generate_tags,
+      lat: data[:latlng][0],
+      lng: data[:latlng][1],
+      overall_rating: generate_rating,
       place_id: ''
   end
 
-  def generate_tags data
-    currencies = data['currencies'].map { |c| c['code'].downcase }
+  def generate_tags
+    currencies = data[:currencies].map { |c| c[:code].downcase }
 
-    currencies.push currencies.include?('usd') ? 'soon' : 'maybe'
+    currencies.push currencies.include?(:usd) ? 'soon' : 'maybe'
   end
 
-  def generate_rating data
-    return 5 if data['currencies'].any? { |c| c['code'] == 'USD' }
+  def generate_rating
+    return 5 if data[:currencies].any? { |c| c[:code] == 'USD' }
 
     rand 1..4
   end
