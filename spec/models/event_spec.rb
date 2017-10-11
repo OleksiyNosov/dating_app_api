527294604193e6c528d8b67671c4c4dec520b545
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-12 02:21:50 +0300

    Update tests due to previous changes

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -10,49 +10,10 @@ RSpec.describe Event, type: :model do
   it { is_expected.to have_many :invites }
 
   it { is_expected.to validate_presence_of :place }
-  
+
   it { is_expected.to validate_presence_of :user }
-  
+
   it { is_expected.to validate_presence_of :title }
 
-  it { is_expected.to define_enum_for(:kind).with([:public_event, :private_event, :friends_only]) }
-
-  describe '#new_invite_if_not_exist' do
-    let(:invited_user) { stub_model User }
-
-    context 'user not exist' do
-      before do
-        #
-        # => invites.find_by
-        #
-        expect(subject).to receive(:invites) do
-          double.tap { |a| expect(a).to receive(:find_by).with(user: invited_user).and_return false }
-        end
-      end
-
-      before do
-        #
-        # => invites.build
-        #
-        expect(subject).to receive(:invites) do
-          double.tap { |a| expect(a).to receive(:build).with(user: invited_user) }
-        end
-      end
-
-      it { expect { subject.new_invite_if_not_exist invited_user }.to_not raise_error }
-    end
-
-    context 'user already exist' do
-      before do
-        #
-        # => invites.find_by
-        #
-        expect(subject).to receive(:invites) do
-          double.tap { |a| expect(a).to receive(:find_by).with(user: invited_user).and_return true }
-        end
-      end
-
-      it { expect { subject.new_invite_if_not_exist invited_user }.to_not raise_error }
-    end
-  end
+  it { is_expected.to define_enum_for(:kind).with(%i[public_event private_event friends_only]) }
 end
