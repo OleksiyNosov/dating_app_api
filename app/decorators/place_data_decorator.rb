e5aa84666fd1590676deb023c092cb0950067bd0
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-12 00:57:47 +0300

    Create PlaceDataDecorator for easy extracion of attributes

---
+++ app/decorators/place_data_decorator.rb
@@ -0,0 +1,30 @@
+class PlaceDataDecorator < Draper::Decorator
+  delegate_all
+
+  def attributes
+    if context[:restcountries_capital]
+      {
+        name: data[:name],
+        city: data[:capital],
+        tags: generate_tags,
+        lat: data[:latlng][0],
+        lng: data[:latlng][1],
+        overall_rating: generate_rating,
+        place_id: ''
+      }
+    end 
+  end
+
+  private
+  def generate_tags
+    currencies = data[:currencies].map { |c| c[:code].downcase }
+
+    currencies.push currencies.include?('usd') ? 'soon' : 'maybe'
+  end
+
+  def generate_rating
+    return 5 if data[:currencies].any? { |c| c[:code] == 'USD' }
+
+    rand 1..4
+  end
+end
